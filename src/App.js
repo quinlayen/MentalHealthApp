c032776f84f1fe0401e6cca018be9bac3e87ed9a
quinlayen <quinlayen@gmail.com>
2018-05-26 19:13:59 -1000

    connected userRegistrationForm for testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import 
+import userRegistrationForm from './components/userRegistrationForm'
+
 
 class App extends Component {
   render() {
