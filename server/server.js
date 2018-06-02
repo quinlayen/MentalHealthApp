2d33a89f406eab905329936fc242fc29ae5f4566
Audrey <audreybrown94@gmail.com>
2018-06-02 09:21:31 -1000

    setting up twilio messaging environment

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+var AccessToken = require('twilio').jwt.AccessToken;
+var Twilio;
 // const routes = require("./routes");
 
 const PORT = process.env.PORT || 8080;
-const providersRoute = require("./routes/care_providers.js");
+const providersRoute = require('./routes/care_providers.js');
 // const authRoute = require("./routes/auth.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", providersRoute);
-
+app.use('/', providersRoute);
+app.post('/sendText', (req, res) => {
+  console.log(req);
+});
 // app.get("/", (req, res) => {
 //   console.log("sanity check");
 //   return res.json("hewwwwwwo");
