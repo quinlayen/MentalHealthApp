cbb78d1f8bcfa3bc760ca06d8ee739563dc19707
quinlayen <quinlayen@gmail.com>
2018-06-07 19:11:30 -1000

    preparing for development pull

--- src/containers/providerList.jsx
+++ src/containers/providerList.jsx
@@ -5,67 +5,61 @@ import List from '@material-ui/core/List';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import compose from 'recompose/compose';
-import {fetchDoctors} from '../actions/index'
+import { fetchDoctors } from '../actions/index';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
-
 const styles = theme => ({
-    root: {
-      width: '100%',
-      maxWidth: '360px',
-      backgroundColor: theme.palette.background.paper,
-    },
-  });
+  root: {
+    width: '100%',
+    maxWidth: '360px',
+    backgroundColor: theme.palette.background.paper
+  }
+});
 
-class ProviderList extends Component{
-    constructor(props){
-        super(props);
-       
+class ProviderList extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    }
+  renderDoctors(doctorData) {
+    return (
+      <Fragment>
+        <ListItem key={doctorData.provider_id} button>
+          <ListItemText primary={doctorData.first_name} />
+          <ListItemText primary={doctorData.last_name} />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary={doctorData.specialties} />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary={doctorData.email} />
+          <ListItemText primary={doctorData.phone} />
+        </ListItem>
+        <Divider />
+      </Fragment>
+    );
+  }
 
-    
-    renderDoctors(doctorData){
-        
-        return (
-            <Fragment>
-            <ListItem key ={doctorData.provider_id} button> 
-                <ListItemText primary={doctorData.first_name}/>
-                <ListItemText primary={doctorData.last_name}/>
-             </ListItem> 
-            <ListItem button>
-            <ListItemText primary={doctorData.specialties}/>
-            </ListItem>
-            <ListItem button>
-            <ListItemText primary={doctorData.email}/>
-            <ListItemText primary={doctorData.phone}/>
-            </ListItem>
-             <Divider />
-             </Fragment>
-        )
-    }
-    
-
-      render(){
-        const {classes} = this.props
-          return(
-              <div className={classes.root}>
-                  <List component="nav">
-                      {this.props.doctors.map(this.renderDoctors)}
-                  </List>
-              </div>
-          )
-      }
-
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <List component="nav">{this.props.doctors.map(this.renderDoctors)}</List>
+      </div>
+    );
+  }
 }
 ProviderList.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+  classes: PropTypes.object.isRequired
+};
 
-function mapStateToProps({doctors}){
-    return {doctors}
+function mapStateToProps({ doctors }) {
+  return { doctors };
 }
-  
-  export default compose(withStyles(styles), connect(mapStateToProps))(ProviderList);
+
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps)
+)(ProviderList);
