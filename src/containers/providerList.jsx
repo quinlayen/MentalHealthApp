8aadb4662b826dbee267b5b44faedefa29d4c387
Audrey <audreybrown94@gmail.com>
2018-06-25 14:07:56 +0800

    provider list rough layout

--- src/containers/providerList.jsx
+++ src/containers/providerList.jsx
@@ -8,44 +8,36 @@ import '../styles/providerList.css';
 class ProviderList extends Component {
   renderDoctors(doctorData) {
     return (
-      <div>
-        <li className="list-group-item" key={doctorData.provider_id}>
-          <div className="card">
-            <div className="card-header">
-              <Link to={'doctors/' + doctorData.provider_id}>
-                <h2>
-                  {doctorData.first_name} {doctorData.last_name}
-                </h2>
-              </Link>
-            </div>
-            <img
-              className="card-img-top img-thumbnail"
-              src={doctorData.image}
-              alt="Card image cap"
-            />
-            <div className="card-body">
-              <h5 className="card-text">{doctorData.location}</h5>
-              <h5 className="card-text">{doctorData.type}</h5>
-              <h5 className="card-text">{doctorData.specialties}</h5>
-            </div>
+      <li className="list-inline-item" key={doctorData.provider_id}>
+        <div className="card" style={{ width: 18 + 'em' }}>
+          <div className="card-header">
+            <Link to={'doctors/' + doctorData.provider_id}>
+              <h2>
+                {doctorData.first_name} {doctorData.last_name}
+              </h2>
+            </Link>
           </div>
-        </li>
-      </div>
+          <img
+            className="card-img-top img-thumbnail"
+            src={doctorData.image}
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h4 className="card-text">{doctorData.location}</h4>
+            <h5 className="card-text">{doctorData.type}</h5>
+            <p className="card-text">{doctorData.specialties}</p>
+          </div>
+        </div>
+      </li>
     );
   }
 
   render() {
     return (
       <div className="container">
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="card" style={{ width: 18 + 'em' }}>
-          <ul className="list-group list-group-flush">
-            {this.props.doctors.map(this.renderDoctors)}
-          </ul>
-        </div>
+        <ul className="list-inline">
+          {this.props.doctors.map(this.renderDoctors)}
+        </ul>
       </div>
     );
   }
