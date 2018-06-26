4e4c4b23f00af9ff9c84d503e5cc7903f4ca866f
Audrey <audreybrown94@gmail.com>
2018-06-26 13:44:34 +0800

    provider list styling

--- src/containers/providerList.jsx
+++ src/containers/providerList.jsx
@@ -6,31 +6,50 @@ import { getDetails } from '../actions/index';
 import '../styles/providerList.css';
 
 class ProviderList extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.doctorHeading = this.doctorHeading.bind(this);
+  // }
+
   renderDoctors(doctorData) {
+    console.log(doctorData, 'in render doctors');
     return (
       <li className="list-inline-item" key={doctorData.provider_id}>
         <div className="card" style={{ width: 18 + 'em' }}>
           <div className="card-header">
             <Link to={'doctors/' + doctorData.provider_id}>
-              <h2>
-                {doctorData.first_name} {doctorData.last_name}
-              </h2>
+              <div className="alert alert-danger" role="alert">
+                <h2 className="alert-link">
+                  {doctorData.first_name} {doctorData.last_name}
+                </h2>
+              </div>
+
+              <img
+                className="card-img-top img-thumbnail"
+                src={doctorData.image}
+                alt="provider"
+              />
             </Link>
           </div>
-          <img
-            className="card-img-top img-thumbnail"
-            src={doctorData.image}
-            alt="Card image cap"
-          />
           <div className="card-body">
-            <h4 className="card-text">{doctorData.location}</h4>
-            <h5 className="card-text">{doctorData.type}</h5>
+            <h5 className="card-text">
+              {doctorData.location}
+              <small className="text-muted"> {doctorData.type}</small>
+            </h5>
             <p className="card-text">{doctorData.specialties}</p>
           </div>
         </div>
       </li>
     );
   }
+
+  // doctorHeading(doctorData) {
+  //   return (
+  //     <h2 className="results" key={doctorData.provider_id}>
+  //       {doctorData.type} in {doctorData.location}
+  //     </h2>
+  //   );
+  // }
 
   render() {
     return (
