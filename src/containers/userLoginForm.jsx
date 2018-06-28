10614eb1bb0d18508b059e3bb5a755fbe0a128ec
Audrey <audreybrown94@gmail.com>
2018-06-28 13:30:56 +0800

    styling login provider and home pages

--- src/containers/userLoginForm.jsx
+++ src/containers/userLoginForm.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { loginAction } from "../actions/index";
-import { bindActionCreators } from "redux";
-import "../styles/userLoginForm.css";
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loginAction } from '../actions/index';
+import { bindActionCreators } from 'redux';
+import '../styles/userLoginForm.css';
+import camp from '../styles/static/bemocs_rei_4th_of_july_dribbble.jpg';
 
 class UserLoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: "",
+      username: '',
+      password: '',
       submitted: false,
-      type: "input"
+      type: 'input'
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,8 +22,8 @@ class UserLoginForm extends Component {
   }
 
   componentDidMount() {
-    if (localStorage.getItem("id")) {
-      this.props.history.push("/login");
+    if (localStorage.getItem('id')) {
+      this.props.history.push('/login');
     }
   }
 
@@ -30,7 +31,7 @@ class UserLoginForm extends Component {
     event.preventDefault();
     event.stopPropagation();
     this.setState({
-      type: this.state.type === "input" ? "password" : "input"
+      type: this.state.type === 'input' ? 'password' : 'input'
     });
   }
 
@@ -42,79 +43,82 @@ class UserLoginForm extends Component {
     event.preventDefault();
 
     this.setState({ submitted: true });
-    if (this.state.username !== "" && this.state.password !== "") {
+    if (this.state.username !== '' && this.state.password !== '') {
       this.props.loginAction(this.state);
       // console.log("LOGGINED IN");
-      this.props.history.push("/");
+      this.props.history.push('/');
     }
   }
 
   render() {
     return (
-      <div className="col-md-6 col-centered">
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-
-        <div className="col-6">
-          <h2>Login</h2>
-          <form name="form" onSubmit={this.handleLogin}>
-            <div
-              className={
-                "form-group" +
-                (this.state.submitted && !this.state.username
-                  ? " has-error"
-                  : "")
-              }
-            >
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                className="form-control"
-                name="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-              {this.state.submitted &&
-                !this.state.username && (
-                  <div className="help-block">Username is Required</div>
-                )}
-            </div>
-            <div
-              className={
-                "form-group" +
-                (this.state.submitted && !this.state.password
-                  ? " has-error"
-                  : "")
-              }
-            >
-              <label htmlFor="password">Password</label>
-              <input
-                type={this.state.type}
-                className="form-control"
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-              <span onClick={this.handleClickShowPassword}>
-                <button className="btn btn-primary btn-sm">
-                  {this.state.type === "input" ? "Hide" : "Show"}
-                </button>
-              </span>
-              {this.state.submitted &&
-                !this.state.password && (
-                  <div className="help-block">Password is Required</div>
-                )}
-            </div>
-            <div className="form-group">
-              <button className="btn btn-primary">Login</button>
-              <Link to="/register" className="btn btn-link">
-                Register
-              </Link>
-            </div>
-          </form>
+      <div className="row">
+        <div className="camp-img">
+          <img src={camp} alt="campground" />
+        </div>
+        <div className="col">
+          <h1 className="greeting">Welcome</h1>
+          <div className="login-form">
+            <h2 className="login">Login</h2>
+            <form name="form" onSubmit={this.handleLogin}>
+              <div
+                className={
+                  'form-group' +
+                  (this.state.submitted && !this.state.username
+                    ? ' has-error'
+                    : '')
+                }
+              >
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+                {this.state.submitted &&
+                  !this.state.username && (
+                    <div className="help-block">Username is Required</div>
+                  )}
+              </div>
+              <div
+                className={
+                  'form-group' +
+                  (this.state.submitted && !this.state.password
+                    ? ' has-error'
+                    : '')
+                }
+              >
+                <label htmlFor="password">Password</label>
+                <div className="pw-row">
+                  <input
+                    type={this.state.type}
+                    className="form-control"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
+                  <span onClick={this.handleClickShowPassword}>
+                    <button className="btn btn-primary btn-sm">
+                      {this.state.type === 'input' ? 'Hide' : 'Show'}
+                    </button>
+                  </span>
+                </div>
+                {this.state.submitted &&
+                  !this.state.password && (
+                    <div className="help-block">Password is Required</div>
+                  )}
+              </div>
+              <div className="form-group">
+                <button className="btn btn-primary">Login</button>
+                <small className="text-muted login"> Need an Account?</small>
+                <Link to="/register" className="btn btn-link">
+                  <strong>Register</strong>
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
