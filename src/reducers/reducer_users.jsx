9fc5676fc3389c40b4804ca813226824df4e6663
ktakesue <ktakesue@aol.com>
2018-06-27 15:34:40 -1000

    shows when registered users are loggined in

--- src/reducers/reducer_users.jsx
+++ src/reducers/reducer_users.jsx
@@ -6,7 +6,7 @@ export default (state = {}, action) => {
   switch (action.type) {
     case REGISTER_USER:
       console.log("IN THE REDUCER", action.payload.data);
-      return { user: action.payload.data };
+      return { isLoggedIn: true, user: action.payload.data };
     case LOGIN_USER:
       console.log("IN THE REDUCER", action.payload.data);
       return { isLoggedIn: true, user: action.payload.data };
