7114c0bcebb84e2fb6c3a4777b82eaa4587d5885
Audrey <audreybrown94@gmail.com>
2018-06-29 11:11:18 +0800

    added scrollreveal on homepage

---
+++ src/components/reveal.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import scrollReveal from './scrollReveal';
+
+export default function reveal(WrappedComponent) {
+  return class RevealEnhancer extends Component {
+    bindRef(c) {
+      this.component = c;
+    }
+    componentDidMount() {
+      console.log('component mounted');
+      const domElement = ReactDOM.findDOMNode(this.component);
+      scrollReveal.reveal(domElement);
+    }
+    render() {
+      const that = this;
+      return (
+        <WrappedComponent
+          ref={function(c) {
+            that.bindRef(c);
+          }}
+        />
+      );
+    }
+  };
+}
